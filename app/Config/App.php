6df3f61bde0fc81c34dbe499354a5263877fb32e
MarcoSenpai <mapm1622@gmail.com>
2022-06-15 09:34:39 -0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://marcosenpai.github.io/ci/';
+    public $baseURL = 'https://marcosenpai.github.io';
 
     /**
      * --------------------------------------------------------------------------
